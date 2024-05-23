71d1b1aa6bc6d4f6890d718c0952f78fdca5b30f
diceron1 <rdice.3d@gmail.com>
2024-05-22 20:40:00 -0400

    Player Character + Space like Movement

--- LostInSpace/Source/LostInSpace/PlayerCharacter.h
+++ LostInSpace/Source/LostInSpace/PlayerCharacter.h
@@ -3,15 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "PaperCharacter.h"
+#include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
-class LOSTINSPACE_API APlayerCharacter : public APaperCharacter
+class LOSTINSPACE_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
 };
