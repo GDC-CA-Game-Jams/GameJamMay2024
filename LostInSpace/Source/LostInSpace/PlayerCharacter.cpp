06784b2229ce062450668b3b1f1369a71187d9ad
Patrick Moffett <Patrick@moffett.dev>
2024-05-27 20:39:24 -0700

    Add Movement to Sprite based character

--- LostInSpace/Source/LostInSpace/PlayerCharacter.cpp
+++ LostInSpace/Source/LostInSpace/PlayerCharacter.cpp
@@ -8,7 +8,6 @@ APlayerCharacter::APlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
